b1bc18c7c3be29977efc3427baf18fb1106ec622
nikola <nikrstic@outlook.com>
2026-09-07 15:48:02 +0200

    domain layer tests

--- src/test/java/com/eventix/ticket/TestcontainersConfiguration.java
+++ src/test/java/com/eventix/ticket/TestcontainersConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.Eventix;
+package com.eventix.ticket;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
